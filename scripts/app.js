d132acb6554adb6219a7b7e02ed21b8e63dcfefa
falak <khairkharu789@gmail.com>
2023-04-07 18:32:06 -0700

    intro and task

---
+++ scripts/app.js
@@ -0,0 +1,49 @@
+// DAY 1 TASK 1 OF JS 6 april
+
+
+//  alert('Hey people')
+//   var username =prompt("Your Name PLease","falak")
+//   var user = "falak"
+//  var fuser = "arif"
+//  console.log("Hey " + user+ " " + fuser)
+
+// var khairun nisa
+// khairunNisa camelCase
+// KhairunNisa PascalCase
+// khairun-nisa snake_case
+
+//  console.log(3+5)
+//    console.log(4-5)
+//     console.log(3*5)
+//  console.log(3/5)
+//   console.log(2**5)
+
+//  var chn = (8 + (7 / 9) - (10 / 7) * (4 - 6)) + 4
+//  console.log(chn)
+
+// var age =+ prompt("Your age PLease")
+// var fage =+ prompt("Your father age PLease")
+// alert(age+fage)
+// alert(Number(age) + Number(fage))
+// console.log(typeof age)
+
+
+// let data= "hello";
+// let data1= "hy";
+// let data3= data + " " + data1;
+// console.log(data3)
+
+// day 2 java task
+
+alert("welcom in zakat calculation")
+
+var zakatPercentage = 0.025;
+
+var userInput = +prompt("Enter Your Amount");
+
+var result = zakatPercentage  * userInput ;
+
+alert("Your Zakat Value is " + result);
+
+
+
